380c2c7b977937d438f20fb9992cfd6d65641d49
thomasRim <thomas.rimme@outlook.com>
2021-03-30 11:44:00 +0200

    Added comments in python files to get started

--- .idea/FMI.py
+++ .idea/FMI.py
@@ -1,1 +1,7 @@
 #FML script
+
+"""
+
+Skal kjøres i CLI! Skal kunne vise all data som ligger i csv-filen, altså hente det fra en metode i storage.
+
+"""
--- .idea/storage.py
+++ .idea/storage.py
@@ -1,1 +1,11 @@
 #storage server process
+
+"""
+
+Denne skal lese inn fra weather_station.py, og lagre dataen i csv-filen.
+Den skal også gi FMI tilgang til dataen som er lagret hvis den får en forespørsel om det.
+
+Står også at alle filene skal kunne kjøres i localhost, litt usikker på hva det menes med, men kanskje bare på pcen?
+
+
+"""
