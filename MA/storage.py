59bf6cf02011477dffb1ebd6319db60e76285a32
erlend223 <erl.end@hotmail.com>
2021-04-03 13:11:06 +0200

    added file to collect all data in one file + some functions to get certain data from file

--- MA/storage.py
+++ MA/storage.py
@@ -16,7 +16,8 @@ import pandas as pd
 import weather_station as ws
 
 messurments = {
-    "month": ["may"],
+    "month": ["may", "may", "june"],
+    "location": ["bergen"],
     "day_of_month": [1, 2, 3, 4, 5, 6, 7],
     "voltage": [5, 2, 6, 3],
     "amp": [2]
@@ -29,22 +30,26 @@ def PUT(messurments: dict):
     month = liste[0].lower()
     dataframe = pd.concat([pd.Series(v, name=k) for k, v in messurments.items()], axis=1)
     dataframe = dataframe.fillna("-")
-    dataframe.to_csv("database\%s.csv" % (month), encoding='utf-8', index=False)
+    #dataframe.to_csv("database\%s.csv" % month,mode ='a', encoding='utf-8', index=False)
+    dataframe.to_csv("database\\allData.csv", mode='a', encoding='utf-8', index=False)
 
 
 def GET(month: str):
-    data = pd.read_csv("database\%s.csv" % (month))
+
+    #data = pd.read_csv("database\%s.csv" % month)
+    data = pd.read_csv("database\\allData.csv")
+    if month == "all":
+        print(data)
     location_data = data.iloc[0, 0]
     month_data = data.iloc[0, 1]
-    dick = data.to_dict()
-    print(dick)
-    data = data.drop(columns=['location', 'month'])
+    contains_month = data[data['month'].str.contains(month)]
+    #contains_month = data.drop(columns=['location', 'month'])
     print("Weather data for %s in %s " % (location_data, month_data))
-    print(data)
+    print(contains_month.reset_index(drop=True))
 
 
-#######################.reset_index(inplace=True)
 
-data = ws.collect_weather_data(10)
-PUT(data)
-GET("may")
+
+# data = ws.collect_weather_data(10)
+PUT(messurments)
+GET('all')
