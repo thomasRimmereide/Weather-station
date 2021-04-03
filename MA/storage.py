7cb40f6522f01ba7b2b62605c1ac2f5e6600d3c5
erlend223 <erl.end@hotmail.com>
2021-04-03 12:07:01 +0200

    moved database folder inside MA

--- MA/storage.py
+++ MA/storage.py
@@ -1,4 +1,4 @@
-#storage server process
+# storage server process
 
 """
 Denne skal lese inn fra weather_station.py, og lagre dataen i csv-filen.
@@ -11,42 +11,40 @@ Første omgang:
 Kunne lagre data fra en dictonary (feltnavn står i station.py) til en CSV fil.
 
 """
-import csv 
+import csv
 import pandas as pd
-import weather_station as ws 
+import weather_station as ws
 
 messurments = {
-"month" : ["may"],
-"day_of_month" : [1,2,3,4,5,6,7],
-"voltage" : [5,2,6,3],
-"amp" : [2]
+    "month": ["may"],
+    "day_of_month": [1, 2, 3, 4, 5, 6, 7],
+    "voltage": [5, 2, 6, 3],
+    "amp": [2]
 
 }
 
-def PUT(messurments : dict):
-    liste =  messurments.get("month")
-    month = liste[0]
-    #col  = [liste[0] for i in range (len(messurments.get("day_of_month")))]
-   # dataframe = pd.DataFrame.from_dict(messurments)#, columns = col)
+
+def PUT(messurments: dict):
+    liste = messurments.get("month")
+    month = liste[0].lower()
     dataframe = pd.concat([pd.Series(v, name=k) for k, v in messurments.items()], axis=1)
     dataframe = dataframe.fillna("-")
-    #dataframe = dataframe.drop(index = "month")
-    dataframe.to_csv("database\%s.csv" % (month), encoding='utf-8' ,index = False)
- 
+    dataframe.to_csv("database\%s.csv" % (month), encoding='utf-8', index=False)
 
-def GET(month : str):
+
+def GET(month: str):
     data = pd.read_csv("database\%s.csv" % (month))
-    location_data = data.iloc[0,0]
-    month_data = data.iloc[0,1]
-    data = data.drop(columns = ['location','month'])
-    print("Weather data for %s in %s " %(location_data, month_data))
-    dictionary = pd.DataFrame.to_dict(data)
+    location_data = data.iloc[0, 0]
+    month_data = data.iloc[0, 1]
+    dick = data.to_dict()
+    print(dick)
+    data = data.drop(columns=['location', 'month'])
+    print("Weather data for %s in %s " % (location_data, month_data))
     print(data)
-    return dictionary
 
-    
 
-#PUT(messurments)
-data= ws.collect_weather_data(30)
+#######################.reset_index(inplace=True)
+
+data = ws.collect_weather_data(10)
 PUT(data)
 GET("may")
