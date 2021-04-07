697267bec7f11ee37b03454f6c927484b93a240c
Vilde <vilde.haugstad@student.uib.no>
2021-04-07 21:04:24 +0200

    Experimenting with html

--- MA/app.py
+++ MA/app.py
@@ -1,58 +1,12 @@
-from flask import Flask
-
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    webpage = \
-        stylesheet + \
-        header() + \
-        body() + \
-        footer()
-    return webpage
+    return render_template('index.html')
 
 
-def header():
-    return f'''
-        <h2>
-        <img src="weather.jpg" alt="weather">
-           test
-        </h2>
-        <br/>
-        <hr/>
-    '''
-
-
-def body():
-    return '''
-        <h3>Test</h3>
-        <br/>
-        <hr/>
-        <p>
-            test
-        </p>
-        <hr/>
-    '''
-
-def footer():
-    return '''
-        <footer>
-            test
-        </footer>
-    '''
-
-
-stylesheet = '''
-    <style>
-        body {
-            font-size: 24px;
-        }
-        footer {
-            margin-top: 30px;
-            padding-top: 10px;
-            border-top: 1px solid black;
-        }
-    </style>
-'''
+if __name__ == '__main__':
+    app.run()
