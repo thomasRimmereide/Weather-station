641d73988cd4b602b5f012b7ece176223a784efe
Vilde <vilde.haugstad@student.uib.no>
2021-04-07 19:43:52 +0200

    Started implementing flask and GUI

--- MA/FMI.py
+++ MA/FMI.py
@@ -14,9 +14,6 @@ weather_station -   client
 
 Gjerne se på tidligere oppgaver:)
 """
-from socket import socket, AF_INET, SOCK_DGRAM
-
-
 
 import socket
 
@@ -37,28 +34,3 @@ while True:
 ClientSocket.close()
 
 
-"""
-def welcome():
-    command = input("which data do you want")
-    storage.get(command)
-"
-
-socket = socket(AF_INET, SOCK_DGRAM)
-
-while {(text := input('> ').lower()) != 'shut down'}:
-    socket.sendto(text.encode(), ('localhost', 55555))
-    msg, addr = socket.recvfrom(2048)
-    print(msg.decode())
-
-def tisstass():
-    sock = socket()
-    sock.connect(storage)
-    request = input("Input: ")  # change variable name
-
-    sock.send(request.encode())
-    new_request = sock.recv(1024).decode()
-    print(f"tisstass")
-    sock.close()
-"""
-
-# welcome()
