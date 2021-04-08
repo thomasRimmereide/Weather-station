b0559c70908254d1fe1ec13d18cafe9631130c15
Vilde <vilde.haugstad@student.uib.no>
2021-04-08 20:06:22 +0200

    Added url to different weather pages

--- GUI/app.py
+++ GUI/app.py
@@ -8,5 +8,12 @@ def index():
     return render_template('index.html')
 
 
+# /city/bergen
+@app.route('/city/<city_name>')
+def get_city_weather_data(city_name):
+    print("City name is " + city_name)
+    return render_template('city.html', city_name=city_name)
+
+
 if __name__ == '__main__':
     app.run()
